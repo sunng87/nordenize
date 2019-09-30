dbc28ef8f7da19046c23b6e5a7ec60f8cd883e62
Ning Sun <sunng@protonmail.com>
2019-10-01 00:04:38 +0800

    (feat) add color selector function

--- src/transformer.ts
+++ src/transformer.ts
@@ -1,6 +1,23 @@
-import * as colorConvert from 'color-convert';
+type VEC3 = [number, number, number];
+type HSL = VEC3;
+type HSL_DISTANCE_FN = (c1: HSL, c2: HSL) => number;
 
-const weightedHslDistanceFn = (weight: [number, number, number]) => (
-    hsl1: [number, number, number],
-    hsl2: [number, number, number],
-) => [1, 2, 3].map(i => (hsl1[i] - hsl2[i]) * weight[i]).reduce((itm, acc) => acc + itm);
+// create a function for calc weighted hsl distance between two hsl color value
+// FIXME: when L is out of certain scope, h and s doesn't mean anything
+export let weightedHslDistanceFn = (weight: VEC3) => (hsl1: HSL, hsl2: HSL) =>
+    [1, 2, 3].map(i => Math.abs(hsl1[i] - hsl2[i]) * weight[i]).reduce((itm, acc) => acc + itm);
+
+export let findBestMatchColor = (givenColor: HSL, presetColors: HSL[], distanceFn: HSL_DISTANCE_FN) => {
+    let min: number = Infinity;
+    let matchedColor: HSL = presetColors[0];
+
+    presetColors.forEach(cur => {
+        const distance = distanceFn(givenColor, cur);
+        if (distance < min) {
+            min = distance;
+            matchedColor = cur;
+        }
+    });
+
+    return matchedColor;
+};
