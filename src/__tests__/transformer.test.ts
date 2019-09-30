800aebc1df0cf8f1351554f77bc37746e9c93c3f
Ning Sun <sunng@protonmail.com>
2019-10-01 00:05:04 +0800

    (test) add test for color selector WIP

---
+++ src/__tests__/transformer.test.ts
@@ -0,0 +1,12 @@
+import { weightedHslDistanceFn, findBestMatchColor } from '../transformer';
+import { colorsHsl, palettes } from '../nord';
+import * as colorConvert from 'color-convert';
+
+test('findBestMatchcolor', () => {
+    let distFn = weightedHslDistanceFn([4, 1, 2]);
+    let red = colorConvert.hex.hsl('#FF1111');
+    expect(findBestMatchColor(red, colorsHsl, distFn)).toStrictEqual(colorConvert.hex.hsl(palettes['Aurora'][0]));
+
+    let white = colorConvert.hex.hsl('#FFFFFF');
+    expect(findBestMatchColor(white, colorsHsl, distFn)).toStrictEqual(colorConvert.hex.hsl(palettes['Snow Storm'][2]));
+});
