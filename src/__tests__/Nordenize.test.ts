34cb06d8cd28a8e7b0755f9b9325990f4eded01f
Ning Sun <sunng@protonmail.com>
2019-10-06 11:52:07 +0800

    (feat) add module integration

--- src/__tests__/Nordenize.test.ts
+++ src/__tests__/Nordenize.test.ts
@@ -1,5 +1,3 @@
 import { Nordenize } from '../index';
 
-test('Nordenize call', () => {
-    expect(Nordenize('Polar Night')).toBe('Hello #2e3440');
-});
+test('Nordenize call', () => {});
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,16 @@
 import * as nord from './nord';
+import { mapStyleColorRules } from './dom';
+import * as transform from './transformer';
+import * as colorConvert from 'color-convert';
 
-export const Nordenize = (name: string) => `Hello ${nord.palettes[name][0]}`;
+export const setup = () => {
+    window.addEventListener('DOMContentLoaded', event => {
+        const distFn = transform.weightedHslDistanceFn([3, 0.5, 2]);
+
+        mapStyleColorRules(color => {
+            const inputHsl = colorConvert.hex.hsl(color);
+            const hsl = transform.findBestMatchColor(inputHsl, nord.colorsHsl, distFn);
+            return colorConvert.hsl.hex(hsl);
+        });
+    });
+};
