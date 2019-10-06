7a2fdc510b3a0fa660b89bf06d15d7df827bf20d
Ning Sun <sunng@protonmail.com>
2019-10-06 15:18:55 +0800

    (refactor) export class

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,8 @@ import { mapStyleColorRules } from './dom';
 import * as transform from './transformer';
 import * as colorConvert from 'color-convert';
 
-export const setup = () => {
-    window.addEventListener('DOMContentLoaded', event => {
+export class Nordenize {
+    transform() {
         const distFn = transform.weightedHslDistanceFn([3, 0.5, 2]);
 
         mapStyleColorRules(color => {
@@ -12,5 +12,11 @@ export const setup = () => {
             const hsl = transform.findBestMatchColor(inputHsl, nord.colorsHsl, distFn);
             return colorConvert.hsl.hex(hsl);
         });
-    });
-};
+    }
+
+    setup() {
+        window.addEventListener('DOMContentLoaded', event => {
+            this.transform();
+        });
+    }
+}
